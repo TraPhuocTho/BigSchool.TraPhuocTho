6fe55c1d34fafc93df64e988e123ce18affdd0ed
Admin <Admin@DESKTOP-56D8F95>
2022-06-16 22:50:23 +0700

    Add project files.

---
+++ BigSchool.TraPhuocTho/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool.TraPhuocTho.Startup))]
+namespace BigSchool.TraPhuocTho
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
