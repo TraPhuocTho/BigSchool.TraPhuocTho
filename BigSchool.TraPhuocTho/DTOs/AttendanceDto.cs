df30c041ebebfb645a655407173a094e154017e8
Trà Thọ <traphuoctho123@gmail.com>
2022-06-24 00:36:48 +0700

    update-database

---
+++ BigSchool.TraPhuocTho/DTOs/AttendanceDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.TraPhuocTho.DTOs
+{
+    public class AttendanceDto
+    {
+        public int CourseId { get; set; }
+
+    }
+    public class FollowingDto
+    {
+        public string FolloweeId { get; set; }
+
+    }
+
+}
